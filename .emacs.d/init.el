6fd43466fd9fc3b4fb72127c0c7f42dbd8197672
emelyans <Sergey.Emelyanenkov@ubs.com>
2017-01-17 11:59:15 -0500

    Add proxy configuration.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,6 +2,8 @@
 
 (add-to-list 'load-path "~/.emacs.d/config")
 
+(load "proxy.el")
+
 (load "package-repositories.el")
 
 (load "look-and-feel.el")
@@ -18,6 +20,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (tango-dark)))
  '(package-selected-packages
    (quote
     (eldoc-eval paredit multiple-cursors evil ztree ace-jump-mode switch-window undo-tree auto-complete smex auto-compile use-package))))
